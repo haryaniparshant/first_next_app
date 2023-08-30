90cd96b6bd857c1125713583b530e6e23ca8efc4
Parshant <parshant4321@gmail.com>
2023-08-30 17:11:53 +0500

    Initail Commit

--- src/app/page.js
+++ src/app/page.js
@@ -23,7 +23,7 @@ export default function Home() {
               width={100}
               height={24}
               priority
-            />
+            /> 
           </a>
         </div>
       </div>
